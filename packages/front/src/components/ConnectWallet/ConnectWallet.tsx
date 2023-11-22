995706be19c8403841e8758a508c331b2b2455d7
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-11-23 01:21:58 +0700

    refactor: Removed ConnectWalletDrawer

--- packages/front/src/components/ConnectWallet/ConnectWallet.tsx
+++ packages/front/src/components/ConnectWallet/ConnectWallet.tsx
@@ -1,17 +1,13 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { useAccount, useDisconnect } from "wagmi";
+import { useAccount } from "wagmi";
 
-import ConnectWalletDrawer from "../ConnectWalletDrawer/ConnectWalletDrawer";
 import Button from "../ui/Button/Button";
 import AccountDrawer from "../AccountDrawer/AccountDrawer";
 
 export default function ConnectWallet() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [isOpenAccount, setIsOpenAccount] = useState<boolean>(false);
-
-    const { disconnect } = useDisconnect();
     const { address, isConnected, isConnecting } = useAccount();
 
     const [isClient, setIsClient] = useState(false)
@@ -21,33 +17,24 @@ export default function ConnectWallet() {
     }, [])
 
     if (!isClient) {
-        return <Button rounded>Connecting...</Button>;
+        return <Button>Connecting...</Button>;
     }
-
-    if (!isConnected) {
-        return (
-            <div>
-                <Button onClick={() => setIsOpen(true)}>
-                    {isConnecting ? 'Connecting...' : 'Connect a Wallet'}
-                </Button>
-                <ConnectWalletDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
-            </div>
-        )
-    }
-
-    const logout = () => {
-        disconnect();
-        setIsOpenAccount(false);
-    };
 
     return (
         <div>
-            <Button onClick={() => setIsOpenAccount(true)} rounded>{address?.slice(0, 15)}</Button>
+            {!isConnected ? (
+                <Button onClick={() => setIsOpen(true)}>
+                    {isConnecting ? 'Connecting...' : 'Connect a Wallet'}
+                </Button>
+            ) : (
+                <Button onClick={() => setIsOpen(true)}>{address?.slice(0, 15)}</Button>
+            )}
 
             <AccountDrawer
-                isOpen={isOpenAccount}
-                onDisconnect={logout}
-                onClose={() => setIsOpenAccount(false)}
+                isConnected={isConnected}
+                isOpen={isOpen}
+                onDisconnect={() => setIsOpen(false)}
+                onClose={() => setIsOpen(false)}
             />
         </div>
     )
