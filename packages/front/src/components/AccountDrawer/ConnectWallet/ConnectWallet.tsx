349da4cb6168852212a66e1baa5e144ac0f42446
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-11-28 11:32:31 +0700

    feat(account): Layout to connection  wallet

--- packages/front/src/components/AccountDrawer/ConnectWallet/ConnectWallet.tsx
+++ packages/front/src/components/AccountDrawer/ConnectWallet/ConnectWallet.tsx
@@ -1,5 +1,12 @@
-import Button from "../../ui/Button/Button";
+import Image from "next/image";
 import { useConnect } from "wagmi";
+
+import Button from "../../ui/Button/Button";
+import styles from './ConnectWallet.module.css';
+
+const ConnectorIcon = {
+    'MetaMask': <Image src="/svg/metamask.svg" width={32} height={32} alt="MetaMask" />
+}
 
 export default function ConnectWallet({ onClose }) {
     const { connect, connectors, isLoading, pendingConnector } =
@@ -8,23 +15,27 @@ export default function ConnectWallet({ onClose }) {
         });
 
     return (
-        <>
-            <h2>Connect a Wallet</h2>
+        <div className={styles.wrapper}>
+            <div>
+                <h2 className={styles.title}>Connect a Wallet</h2>
 
-            {connectors.map((connector) => (
-                <Button
-                    disabled={!connector.ready}
-                    key={connector.id}
-                    onClick={() => connect({ connector })}
-                    block
-                >
-                    {connector.name}
-                    {!connector.ready && ' (unsupported)'}
-                    {isLoading &&
-                        connector.id === pendingConnector?.id &&
-                        ' (connecting)'}
-                </Button>
-            ))}
-        </>
+                {connectors.map((connector) => (
+                    <Button
+                        disabled={!connector.ready}
+                        key={connector.id}
+                        className={styles.connectorBtn}
+                        onClick={() => connect({ connector })}
+                        block
+                    >
+                        {ConnectorIcon[connector.name]}
+                        {connector.name}
+                        {!connector.ready && ' (unsupported)'}
+                        {isLoading &&
+                            connector.id === pendingConnector?.id &&
+                            ' (connecting)'}
+                    </Button>
+                ))}
+            </div>
+        </div>
     )
 }
