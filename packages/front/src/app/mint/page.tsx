3cbdd1fba278e90c0c9569a1ff867b7b70f78d8c
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-11-28 13:41:06 +0700

    feat(mint): base logic of mint

--- packages/front/src/app/mint/page.tsx
+++ packages/front/src/app/mint/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useCallback, useState } from "react";
+import { useAccount, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import { message } from 'antd';
 import Image from "next/image";
 
 import styles from './page.module.css';
@@ -12,24 +14,49 @@ import { CreateMintDto, MintDto } from "../../common/MintDto";
 import ApiService from "../../services/ApiService";
 
 export default function Page() {
+    const [messageApi, contextHolder] = message.useMessage();
     const [isMinted, setIsMinted] = useState<boolean>(false);
     const [nft, setNft] = useState<MintDto>();
 
-    const handleGetMint = useCallback(async (data: MintFormData) => {
-        /*const response = await ApiService.createMint(data.file, {
-            name: data.name,
-            description: data.description,
-            userId: 1
-        });*/
+    const { address } = useAccount();
+    const { config } = usePrepareContractWrite({
+        address,
+        abi: [
+            {
+                name: 'mint',
+                type: 'function',
+                stateMutability: 'nonpayable',
+                inputs: [],
+                outputs: [],
+            },
+        ],
+        functionName: 'mint',
+    });
+    const { data, write, writeAsync, error } = useContractWrite(config);
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
 
-        setNft({
-            id: 1,
-            name: 'Fox Geometric',
-            description: 'Description Description Description',
-            imageHash: 'QmdUqFGTunepKfuG9QSTATgP84ox3bSxi7RS3PzosStB1t'
-        });
-        setIsMinted(true)
-    }, []);
+    const handleGetMint = useCallback(async (data: MintFormData) => {
+        try {
+            await writeAsync();
+            /*const response = await ApiService.createMint(data.file, {
+                name: data.name,
+                description: data.description,
+                userId: 1
+            });*/
+
+            setNft({
+                id: 1,
+                name: 'Fox Geometric',
+                description: 'Description Description Description',
+                imageHash: 'QmdUqFGTunepKfuG9QSTATgP84ox3bSxi7RS3PzosStB1t'
+            });
+            setIsMinted(true);
+        } catch (e) {
+            messageApi.warning('Error: User rejected the request');
+        }
+    }, [write]);
 
     if (isMinted) {
         return (
@@ -41,18 +68,23 @@ export default function Page() {
             )}>
                 <PinataImage hash={nft.imageHash} name={nft.name} />
                 {nft.description && <p>{nft.description}</p>}
+                <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
             </Card>
         )
     }
 
     return (
-        <Card className={styles.page} title={(
-            <div className={styles.title}>
-                <span>Mint</span>
-                <CostLabel cost={20} large />
-            </div>
-        )}>
-            <MintForm onSubmit={handleGetMint} />
-        </Card>
+        <>
+            {contextHolder}
+
+            <Card className={styles.page} title={(
+                <div className={styles.title}>
+                    <span>Mint</span>
+                    <CostLabel cost={20} large />
+                </div>
+            )}>
+                <MintForm onSubmit={handleGetMint} />
+            </Card>
+        </>
     )
 }
