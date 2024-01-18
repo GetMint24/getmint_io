fd0450c854772f119aebc27af3f3eb8b457b5ac7
Dmitry Batukhtin <batukhtin@arview.pro>
2024-01-18 15:41:54 +0700

    added auto-generate image for mint

--- environment.d.ts
+++ environment.d.ts
@@ -6,6 +6,10 @@ declare namespace NodeJS {
         readonly TWITTER_CLIENT_ID: string;
         readonly TWITTER_CLIENT_SECRET: string;
         readonly TWITTER_CODE_CHALLENGE: string;
+        readonly S3_ACCESS_KEY: string;
+        readonly S3_SECRET_KEY: string;
+        readonly S3_ENDPOINT?: string;
+        readonly S3_REGION?: string;
         readonly APP_URL: string;
         readonly DATABASE_URL: string;
     }
