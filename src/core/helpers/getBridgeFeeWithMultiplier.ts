7a041bada700bd32042418ffd7f2603a0daa86d9
Artem <stepanov@arview.pro>
2024-04-13 16:15:24 +0400

    feat: change bridge fee

--- src/core/helpers/getBridgeFeeWithMultiplier.ts
+++ src/core/helpers/getBridgeFeeWithMultiplier.ts
@@ -1,25 +1,31 @@
-import { NetworkName } from "../../common/enums/NetworkName"
+import { NetworkName } from '../../common/enums/NetworkName';
 
-const DEFAULT_MULTIPLIER = 50
+const DEFAULT_MULTIPLIER = 50;
 
 const multiplierByNetworkName: Record<NetworkName, number> = {
-    [NetworkName.Base]: DEFAULT_MULTIPLIER, 
-    [NetworkName.ArbitrumNova]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Arbitrum]: 20, 
-    [NetworkName.Avalanche]: DEFAULT_MULTIPLIER, 
-    [NetworkName.LineaMainnet]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Mantle]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Optimism]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Polygon]: DEFAULT_MULTIPLIER, 
-    [NetworkName.PolygonzkEVM]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Scroll]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Zora]: DEFAULT_MULTIPLIER, 
-    [NetworkName.ZkSync]: DEFAULT_MULTIPLIER, 
-    [NetworkName.BSC]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Celo]: DEFAULT_MULTIPLIER, 
-    [NetworkName.Gnosis]: DEFAULT_MULTIPLIER, 
-}
+  [NetworkName.Base]: DEFAULT_MULTIPLIER,
+  [NetworkName.ArbitrumNova]: DEFAULT_MULTIPLIER,
+  [NetworkName.Arbitrum]: 20,
+  [NetworkName.Avalanche]: DEFAULT_MULTIPLIER,
+  [NetworkName.LineaMainnet]: DEFAULT_MULTIPLIER,
+  [NetworkName.Mantle]: DEFAULT_MULTIPLIER,
+  [NetworkName.Optimism]: DEFAULT_MULTIPLIER,
+  [NetworkName.Polygon]: DEFAULT_MULTIPLIER,
+  [NetworkName.PolygonzkEVM]: DEFAULT_MULTIPLIER,
+  [NetworkName.Scroll]: DEFAULT_MULTIPLIER,
+  [NetworkName.Zora]: DEFAULT_MULTIPLIER,
+  [NetworkName.ZkSync]: DEFAULT_MULTIPLIER,
+  [NetworkName.BSC]: DEFAULT_MULTIPLIER,
+  [NetworkName.Celo]: DEFAULT_MULTIPLIER,
+  [NetworkName.Gnosis]: DEFAULT_MULTIPLIER,
+};
 
 export const getBridgeFeeWithMultiplier = (nativeFee: bigint, networkName: NetworkName) => {
-    return nativeFee * BigInt(100 + multiplierByNetworkName[networkName]) / BigInt(100)
-}
+  if (networkName === NetworkName.Arbitrum) {
+    return (nativeFee * BigInt(100 + multiplierByNetworkName[networkName])) / BigInt(100);
+  }
+
+  return 300_000;
+
+  //   return (nativeFee * BigInt(100 + multiplierByNetworkName[networkName])) / BigInt(100);
+};
