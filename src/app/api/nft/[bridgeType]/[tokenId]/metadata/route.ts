55536a5f03f6fc89137060acd2dd045707f89e6c
Alexey Abramov <abramovalekcei@gmail.com>
2024-05-05 12:28:06 +0700

    Metada for nft with other bridge types

--- src/app/api/nft/[bridgeType]/[tokenId]/metadata/route.ts
+++ src/app/api/nft/[bridgeType]/[tokenId]/metadata/route.ts
@@ -1,19 +1,34 @@
-import prisma from "../../../../../utils/prismaClient";
+import prisma from "../../../../../../utils/prismaClient";
+import { BridgeType } from "../../../../../../common/enums/BridgeType";
 
 const notFound = () => Response.json({ message: 'Not Found' }, {
     status: 404
 });
 
-export async function GET(_: Request, route: { params: { tokenId: string } }) {
+const notFoundBridge = () => Response.json({ message: 'Not Found Bridge' }, {
+    status: 404
+});
+
+export async function GET(_: Request, route: { params: { bridgeType: string, tokenId: string } }) {
+    const bridgeType = route.params.bridgeType;
     const tokenId = route.params.tokenId;
 
+    if (!bridgeType || 
+        (bridgeType != "layerzero" && 
+        bridgeType != "hyperlane") ) {
+        return notFoundBridge();
+    }
+
+    var currNetworkType = bridgeType === "layerzero" ? BridgeType.LayerZero : BridgeType.Hyperlane;
+    
     if (!tokenId || !Number.isInteger(parseInt(tokenId))) {
         return notFound();
     }
 
     const nft = await prisma.nft.findFirst({
         where: {
-            tokenId: parseInt(route.params.tokenId)
+            tokenId: parseInt(route.params.tokenId),
+            networkType: currNetworkType
         },
         include: {
             chain: true
